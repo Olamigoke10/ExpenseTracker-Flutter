37851c49fea8db559bf0577e9740b06113544b7c
MRBOX <olamigokeadebayo90@gmail.com>
2025-05-19 17:20:15 +0100

    ShowModalBox-- Adding and Handling text input

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,4 +1,5 @@
 import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
+import 'package:expense_tracker/widgets/new_expense.dart';
 import 'package:flutter/material.dart';
 import 'package:expense_tracker/models/expense.dart';
 
@@ -13,6 +14,13 @@ class Expenses extends StatefulWidget{
 
 
 class _ExpensesState extends State<Expenses>{
+
+  void _openAddExpenseOverlay(){
+    showModalBottomSheet(
+      context: context,
+       builder: (ctx) => const NewExpense(),
+      );
+  }
 
   final List<Expense> _registersExpenses = [
     Expense(
@@ -34,10 +42,10 @@ class _ExpensesState extends State<Expenses>{
   Widget build(BuildContext context) {
     return  Scaffold(
       appBar: AppBar(
-        title: constText("Flutter Expense Tracker"),
+        title: const Text("Flutter Expense Tracker"),
         actions: [
           IconButton(
-            onPressed: () {},
+            onPressed: _openAddExpenseOverlay,
              icon: const Icon(Icons.add)),
         ],
       ),
