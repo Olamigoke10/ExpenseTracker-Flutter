528af970ae1ed41ff62499e64229cff3e80e0916
MRBOX <olamigokeadebayo90@gmail.com>
2025-05-27 20:29:37 +0100

    Using textEditcontroller to save text and editing the textfield to accpet numbers

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 class NewExpense extends StatefulWidget{
   const NewExpense({super.key});
 
+
+
   @override
   State<StatefulWidget> createState() {
    return _NewExpenseState();
@@ -12,18 +14,64 @@ class NewExpense extends StatefulWidget{
 
 class _NewExpenseState extends State<NewExpense>{
 
+  final _titleController = TextEditingController();
+
+  final _amountController = TextEditingController();
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _amountController.dispose();
+    super.dispose();
+  }
+
+  void closeModalBox(){
+  }
+
   @override
   Widget build(BuildContext context) {
     return  Padding(
-      padding: EdgeInsets.all(16),
+      padding: const EdgeInsets.all(16),
       child: Column(
         children: [
           TextField(
+            controller: _titleController,
             maxLength: 50,
-            decoration: InputDecoration(
+            decoration: const InputDecoration(
               label: Text("Title")
             ),
           ),
+
+          TextField(
+            controller: _amountController,
+            keyboardType: TextInputType.number,
+            decoration: const InputDecoration(
+              prefixText: "\$ ",
+              label: Text("Amount"),
+            ),
+          ),
+          Row(
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                print(
+                  _titleController.text,
+                );
+                print(
+                  _amountController.text,
+                );
+                },
+                 child: const Text("Save Expense")
+                 ),
+
+                 TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: const Text("Cancel")
+                  ),
+            ],
+          )
         ],
       ),
     );
